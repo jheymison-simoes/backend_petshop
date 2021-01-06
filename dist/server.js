24d6d1a2d152abe5993ec9938f89525c579ea511
Jheymison <jheymisonbao@live.com>
2021-01-06 17:21:28 -0300

    Alterando

--- dist/server.js
+++ dist/server.js
@@ -3,6 +3,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
+const dotenv_1 = __importDefault(require("dotenv"));
 const express_1 = __importDefault(require("express"));
 require("express-async-errors");
 const cors_1 = __importDefault(require("cors"));
@@ -14,4 +15,5 @@ app.use(cors_1.default());
 app.use(express_1.default.json());
 app.use(routes_1.default);
 app.use(handler_1.default);
+dotenv_1.default.config();
 app.listen(process.env.PORT || 8080);
